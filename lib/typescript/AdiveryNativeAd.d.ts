9b3531dd698377882807bd7b8ca64661dad74f2c
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2024-05-09 01:22:50 +0330

    u

--- lib/typescript/AdiveryNativeAd.d.ts
+++
@@ -1,9 +0,0 @@
-import { NativeAd } from 'adivery';
-import type { ReactElement } from 'react';
-declare type Props = {
-    placementId: string;
-    child: (ad: NativeAd) => ReactElement;
-    fallback: ReactElement;
-};
-declare const AdiveryNativeAd: ({ placementId, child, fallback }: Props) => JSX.Element;
-export default AdiveryNativeAd;
--- lib/typescript/banner-size.d.ts
+++
@@ -1,5 +0,0 @@
-declare const Banner = 0;
-declare const SmartBanner = 1;
-declare const LargeBanner = 2;
-declare const MediumRectangle = 3;
-export { Banner, SmartBanner, LargeBanner, MediumRectangle };
